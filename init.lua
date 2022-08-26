3807679eae4235ad231aad5481bd11a747c95848
KAI <jhongkaihuang@gmail.com>
2022-08-26 20:19:56 +0800

    enable draw sprite

--- init.lua
+++ init.lua
@@ -33,16 +33,16 @@ Config.EnableOutline = false
 Config.Toggle = false
 
 -- Draw a Sprite on the center of a PolyZone to hint where it's located
-Config.DrawSprite = false
+Config.DrawSprite = true
 
 -- The default distance to draw the Sprite
-Config.DrawDistance = 10.0
+Config.DrawDistance = 3.0
 
 -- The color of the sprite in rgb, the first value is red, the second value is green, the third value is blue and the last value is alpha (opacity). Here is a link to a color picker to get these values: https://htmlcolorcodes.com/color-picker/
 Config.DrawColor = {255, 255, 255, 255}
 
 -- The color of the sprite in rgb when the PolyZone is targeted, the first value is red, the second value is green, the third value is blue and the last value is alpha (opacity). Here is a link to a color picker to get these values: https://htmlcolorcodes.com/color-picker/
-Config.SuccessDrawColor = {30, 144, 255, 255}
+Config.SuccessDrawColor = {5, 241, 178, 255}
 
 -- The color of the outline in rgb, the first value is red, the second value is green, the third value is blue and the last value is alpha (opacity). Here is a link to a color picker to get these values: https://htmlcolorcodes.com/color-picker/
 Config.OutlineColor = {255, 255, 255, 255}
@@ -51,7 +51,7 @@ Config.OutlineColor = {255, 255, 255, 255}
 Config.EnableDefaultOptions = false
 
 -- Disable the target eye whilst being in a vehicle
-Config.DisableInVehicle = false
+Config.DisableInVehicle = true
 
 -- Key to open the target eye, here you can find all the names: https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
 Config.OpenKey = 'LMENU' -- Left Alt
